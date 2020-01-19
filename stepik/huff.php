871d13545d45460d4735d02ebb7b52aa14cc2001
dihauti <kostopravbt@gmail.com>
2020-01-19 06:14:03 +0300

    code_huff

--- stepik/huff.php
+++ stepik/huff.php
@@ -1,43 +1,90 @@
 <?php
 
-$inputStr = 'abacabad';
+class Node
+{
+    public string $code;
 
-//first step
-$arr = str_split($inputStr);
-$symbolRate = array_count_values($arr);
-$numberUniqueSymbol = count($symbolRate);
-
-$pq = new SplPriorityQueue();
-foreach($symbolRate as $k=>$v){
-  $pq->insert($k, $v);
+    public function code($code)
+    {
+        $this->code = $code;
+    }
 }
 
-var_dump($pq);
+final class Leaf extends Node
+{
+    public $char;
+    public function __construct($char)
+    {
+        $this->char = $char;
+    }
 
-//second step
+    public function code($code){
+        parent::code($code);
+        print_r($this->char . ":" . $code . PHP_EOL);
+    }
+}
 
+final class InternalNode extends Node
+{
+    public Node $left;
+    public Node $right;
 
-/*
- * a - 4
- * b - 2
- * c - 1
+    public function __construct($left, $right)
+    {
+        $this->left = $left;
+        $this->right = $right;
+    }
+
+    public function code($code)
+    {
+        parent::code($code);
+        $this->left->code($code . '1');
+        $this->right->code($code . '0');
+    }
+}
+
+function code_huff($inputStr)
+{
+    //first step
+    $arr = str_split($inputStr);
+    $symbolRate = array_count_values($arr);
+    $numberUniqueSymbol = count($symbolRate);
+
+    $pq = new SplPriorityQueue();
+    foreach($symbolRate as $k=>$v){
+        $pq->insert([new Leaf($k), $v], -$v);
+    }
+
+    while(count($pq) > 1){
+        $last = $pq->extract();
+        $next = $pq->extract();
+        $pq->insert([new InternalNode($last[0], $next[0]), $last[1]+$next[1]], -$next[1]);
+    }
+    print_r($pq);
+    $root = $pq->extract()[0];
+    $root->code("");
+}
+
+$str = trim(fgets(STDIN));
+code_huff($str);
+
+/**
+ * abdc
+ *
+ * a - 0
+ *
+ * bdc - 1
+ *
+ * b - 1
+ *
+ * dc - 0
+ *
  * d - 1
+ * c - 0
  *
- * next step
- *
- * d, c, b, a
- *
- * next step
- *
- * d + c  = 2 - первый узел
- *
- * (d + c) + b = 4 - второй узел
- *
- * ((d+c)+b) + a = 8 - третий узел
- *
- * a = 0
- * b = 10
- * c = 110
- * d = 111
+ * a:0;
+ * b:10;
+ * c:110
+ * d:111
  *
  */
